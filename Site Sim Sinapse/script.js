203e9def1da4ce6b68fccf58175aa518daabb674
Yasmin Bianca <yasminbiancacruz@gmail.com>
2026-03-10 21:27:03 -0300

    Delete Site Sim Sinapse directory

--- Site Sim Sinapse/script.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu(){
-  let menu = document.querySelector("nav"); // seleciona o nav
-  if(menu.style.display === "flex"){
-    menu.style.display = "none";
-  }else{
-    menu.style.display = "flex";
-  }
-}
